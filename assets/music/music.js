cf9889d643e1f05417156de6d30afbd0c4e9e0e2
LeQuangThanh69420 <none>
2024-02-15 03:37:24 +0700

    update

---
+++ assets/music/music.js
@@ -0,0 +1,7 @@
+const music = [
+    { name: "Usagi Flap", url: "./assets/music/Usagi Flap.mp3", author: "Nor"},
+    { name: "Ghost City Tokyo", url: "./assets/music/Ghost City Tokyo.mp3", author: "Yoasobi"},
+    { name: "Chocolate Lemon", url: "./assets/music/Chocolate Lemon.mp3", author: "Mitsukiyo(ミツキヨ)"},
+    { name: "SecondWarning", url: "./assets/music/SecondWarning.mp3", author: "ProjectMoon"},
+    { name: "Shelter", url: "./assets/music/Shelter.mp3", author: "Porter Robinson"},
+]
